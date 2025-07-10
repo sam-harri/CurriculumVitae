5ce9f89382e88b68d78e0966738bf0acd9e39f1f
Samuel Harrison <samy4home@gmail.com>
2025-07-09 23:52:51 -0400

    feat : blog

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import raw from 'vite-plugin-raw'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    raw({
-      match: /\.md$/,
-    })
   ],
 })
